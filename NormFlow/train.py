ba4b95c67a35f9ae75383c3ff56f7942a2cfd62e
JimmyEsmaily <esmaily.jamal@gmail.com>
2024-08-08 17:55:13 +0200

    from pc

--- NormFlow/train.py
+++ NormFlow/train.py
@@ -5,17 +5,17 @@ from utils import *
 ## Params
 d, k = 2, 1
 n_samples = 1024
-epochs = 10000
+epochs = 1000
 batch_size = 128
 ##
 model = R_NVP(d, k, hidden=100)
-optim = torch.optim.Adam(model.parameters(), lr=1e-3)
+optim = torch.optim.Adam(model.parameters(), lr=0.1)
 scheduler = torch.optim.lr_scheduler.ExponentialLR(optim, 0.999)
 # x_data = torch.randn(n_samples, 2)
 # target_data = torch.tensor(target_distribution(n_samples), dtype=torch.float32)
 
 losses = []
-for _ in range(epochs):
+for ep in range(epochs):
     # get batch
     X, _ = datasets.make_moons(n_samples=batch_size, noise=.05)
     X = torch.from_numpy(StandardScaler().fit_transform(X)).float()
@@ -28,5 +28,8 @@ for _ in range(epochs):
     loss.backward()
     optim.step()
     scheduler.step()
+
+    if ep % 100 == 0:
+        print(f'Epoch {ep}, Loss: {loss.item()}')
 
 view(model, losses)
